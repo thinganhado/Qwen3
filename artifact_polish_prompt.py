fbb7262ecb60b8462e58b7687f226298e43c72e7
thinganhado <167838318+thinganhado@users.noreply.github.com>
2026-02-19 22:22:19 +1100

    changed model

--- artifact_polish_prompt.py
+++ artifact_polish_prompt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 import argparse
 import csv
 import json
@@ -16,7 +16,7 @@ DEFAULT_USER_TEMPLATE_FILE = THIS_DIR / "polisher_prompt" / "region_forensics_us
 DEFAULT_INPUT_QWEN3_VL_ROOT = Path("/datasets/work/dss-deepfake-audio/work/data/datasets/interspeech/En/captioner/")
 DEFAULT_GT_CSV = Path("/datasets/work/dss-deepfake-audio/work/data/datasets/interspeech/img/region_phone_table_grid.csv")
 DEFAULT_OUTPUT_DIR = Path("/datasets/work/dss-deepfake-audio/work/data/datasets/interspeech/En/polisher/")
-DEFAULT_MODEL_ID = "/datasets/work/dss-deepfake-audio/work/data/datasets/interspeech/LLM/Qwen3-4B-Instruct-2507-4bit-GPTQ"
+DEFAULT_MODEL_ID = "/datasets/work/dss-deepfake-audio/work/data/datasets/interspeech/LLM/Qwen3-4B-Instruct-2507"
 
 
 def _load_text_file(path: Path, field_name: str) -> str:
@@ -593,5 +593,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
